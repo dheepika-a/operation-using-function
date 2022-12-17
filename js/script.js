06c9a3c5e8e5756e96d2486e8819d6ca33da46d5
unknown <dheepi117@gmail.com>
2022-12-17 12:02:37 +0530

    add problem statement.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function name(parameter)
+{
+	statement;
+	return expressrion;
+}
+console.log(name)*/
+
+/*
+function addition(c,d)
+{
+	return(c+d);
+}
+function subtraction(c,d)
+{
+	return(c-d);
+}
+function multiplication(c,d)
+{
+	return(c*d);
+}
+function division(c,d)
+{
+	return(c/d);
+}
+
+var a=parseInt(prompt("enter the a value"));
+var b=parseInt(prompt("enter the b value")); 
+var operation=prompt("enter the process"); 
+
+if(operation=="addition")
+{
+	var x=addition(a,b);
+	console.log(x);
+
+}
+
+else if(operation=="subtraction")
+{
+	var x=subtraction(a,b);
+	console.log(x);
+
+}
+else if(operation=="multiplication")
+{
+	var x=multiplication(a,b);
+	console.log(x);
+
+}
+else
+{
+	var x=division(a,b);
+	console.log(x);
+
+}*/
